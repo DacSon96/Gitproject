9cd5fa0731bb4495e61b352e32a8c171a9a41d74
DacSon96 <86951228+DacSon96@users.noreply.github.com>
2021-07-08 15:55:21 +0700

    alo alo

---
+++ java/toantu.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("enter the Width");
+inputHeight = prompt("Enter the Height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is:" + area)
+
